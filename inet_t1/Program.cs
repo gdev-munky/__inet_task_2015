680f167a4a5aa308e5a792527304fe71136aba71
Munky <ml4_aem@mail.ru>
2015-04-03 13:18:18 +0500

    task1 initial

---
+++ inet_t1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace inet_t1
+{
+    class Program
+    {
+        private static WhoIs whois;
+        static void Main(string[] args)
+        {
+            const string whoisDb = "whois_db.txt";
+            var rebuild = false;
+            if (!File.Exists(whoisDb))
+                rebuild = true;
+            else if ((DateTime.Now - File.GetLastWriteTime(whoisDb)).Days >= 7)
+                rebuild = true;
+
+            Console.WriteLine("Loading whois db from {0}...", rebuild ? "web" : "disk");
+            whois = new WhoIs(rebuild);
+            foreach (var s in args)
+            {
+                try
+                {
+                    IPAddress ip;
+                    if (!IPAddress.TryParse(s, out ip))
+                    {
+                        var entry = Dns.GetHostEntry(s);
+                        ip = entry.AddressList.First();
+                    }
+                    TraceRt(ip);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            Console.WriteLine("Done!");
+        }
+
+        static void TraceRt(IPAddress ip, int ttl = 30, int packets = 1)
+        {
+            Console.WriteLine("Tracing route to ip : {0} ...", ip);
+            for (var step = 0; step < ttl; ++step)
+            {
+                using (var pinger = new Ping())
+                {
+                    var time = DateTime.Now;
+                    var reply = pinger.Send(ip, 5000, new byte[] {}, new PingOptions(step + 1, false));
+                    var dtime = (DateTime.Now - time).TotalMilliseconds;
+                    if (reply == null)
+                    {
+                        Console.WriteLine("[{0,2}] : no reply", step);
+                        continue;
+                    }
+                    if (reply.Status != IPStatus.Success && reply.Status != IPStatus.TtlExpired )
+                    {
+                        Console.WriteLine("[{0,2}] : error={1}", step, reply.Status);
+                        continue;
+                    }
+                    Console.Write("[{0,2}] : ip={1,15}; time={2} ms; country: ", step, reply.Address, Math.Round(dtime));
+                    Console.WriteLine(whois.CountryByIP(reply.Address));
+
+                    if (Equals(ip, reply.Address))
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ inet_t1/whois.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace inet_t1
+{
+    public class WhoIs
+    {
+        private string[] db;
+        public WhoIs(bool rebuild = false)
+        {
+            if (rebuild) BuildDB();
+            db = LoadDB();
+        }
+
+        public string CountryByIP(IPAddress ip)
+        {
+            var whois = QueryWhoisByIP(ip);
+            if (whois == "#")
+                return "unknown";
+            return ExtractCountry(QueryWhoIs(ip, whois));
+        }
+
+        public string QueryWhoIs(IPAddress ip, string whoisServer)
+        {
+            var ipstr = ip.ToString();
+            if (!ipstr.EndsWith("\r\n"))
+                ipstr += "\r\n";
+            var provider = QueryWhoisByIP(ip);
+            if (provider == "whois.arin.net")
+                ipstr = "n " + ipstr;
+            return Query(whoisServer, ipstr);
+        }
+
+        static string Query(string provider, string query)
+        {
+            var mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            mainSocket.ReceiveTimeout = 1000;
+            mainSocket.Connect(provider, 43);
+
+            recvAll(mainSocket);
+
+            Thread.Sleep(500);
+            mainSocket.Send(Encoding.UTF8.GetBytes(query));
+            Thread.Sleep(500);
+
+            var data = recvAll(mainSocket);
+            mainSocket.Close();
+            return Encoding.UTF8.GetString(data);
+        }
+
+        static byte[] recvAll(Socket socket)
+        {
+            var result = new List<byte>();
+            var sockList = new List<Socket> { socket };
+            var emptyList = new List<Socket>();
+            Socket.Select(sockList, emptyList, emptyList, 500);
+            while (sockList.Any())
+            {
+                var data = new byte[0xffff];
+                var recvd = socket.Receive(data);
+                if (recvd == 0)
+                    break;
+                for (var i = 0; i < recvd; ++i)
+                {
+                    result.Add(data[i]);
+                }
+            }
+            return result.ToArray();
+        }
+
+        static void BuildDB()
+        {
+            var wc = new WebClient();
+            var dbStr = wc.DownloadString(
+                "http://www.internetassignednumbersauthority.org/assignments/ipv4-address-space/ipv4-address-space.txt");
+
+            var db = dbStr.Split('\n');
+            var result = new string[256];
+
+            for (var id = 0; id < 256; ++id)
+            {
+                result[id] = "#";
+            }
+
+            foreach (var line in db)
+            {
+                if (line.Length < 80)
+                    continue;
+                var idStr = line.Substring(3, 3);
+                int id = -1;
+                if (!int.TryParse(idStr, out id)) continue;
+                if (id < 0 || id > 255) continue;
+
+                if (!line.Contains("whois."))
+                    continue;
+                var tmp = line.Substring(line.IndexOf("whois."), 17);
+                result[id] = tmp.Trim();
+            }
+
+            var f = new StreamWriter("whois_db.txt");
+            foreach (var s in result)
+            {
+                f.WriteLine(s);
+            }
+            f.Close();
+        }
+
+        static string[] LoadDB()
+        {
+            var result = new string[256];
+            var f = new StreamReader("whois_db.txt");
+            for (var i = 0; i < result.Length; i++)
+            {
+                result[i] = f.ReadLine();
+            }
+            f.Close();
+            return result;
+        }
+
+        string QueryWhoisByIP(IPAddress ip)
+        {
+            return db[ip.GetAddressBytes()[0]];
+        }
+
+        static string ExtractCountry(string answer)
+        {
+            var lines = answer.Split('\n');
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("country: "))
+                {
+                    return line.Substring("country: ".Length).Trim();
+                }
+                if (line.StartsWith("Country: "))
+                {
+                    return line.Substring("country: ".Length).Trim();
+                }
+            }
+            return "unknown";
+        }
+    }
+}
