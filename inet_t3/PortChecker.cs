b30301198e9f067781ff349c7cedc4b83f420041
Munky <ml4_aem@mail.ru>
2015-04-05 14:14:46 +0500

    [t3]

---
+++ inet_t3/PortChecker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace inet_t3
+{
+    public class PortChecker
+    {
+        public PortChecker(IPAddress ip, int timeout = 400)
+        {
+            IP = ip;
+            TimeOut = timeout;
+        }
+        public IPAddress IP { get; set; }
+        public int TimeOut { get; set; }
+        public bool IsTcpOpened(int port)
+        {
+            if (IP == null)
+                throw new NullReferenceException("IP is set to null, cannot do anything");
+            
+            var endPoint = new IPEndPoint(IP, port);
+            var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var result = sock.BeginConnect(endPoint, (ar =>
+                {
+                    try
+                    {
+                        var client = (Socket)ar.AsyncState;
+                        client.EndConnect(ar);
+                    }
+                    catch { }
+                }), sock);
+            var success = result.AsyncWaitHandle.WaitOne(TimeOut, false);
+            sock.Close();
+            return success;
+        }
+
+        public bool IsUdpOpened(int port)
+        {
+            if (IP == null)
+                throw new NullReferenceException("IP is set to null, cannot do anything");
+            EndPoint ep = new IPEndPoint(IP, port);
+            EndPoint recvEP = new IPEndPoint(IP, port);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var sendbytes = Encoding.ASCII.GetBytes("Just scanning you");
+            socket.ReceiveTimeout = 
+            socket.SendTimeout = 400;
+            var i = socket.SendTo(sendbytes, ep);
+            var buffer = new byte[10240];
+            try
+            {
+                var recvd = socket.ReceiveFrom(buffer, ref recvEP);
+            }
+            catch (SocketException ex)
+            {
+                if (ex.SocketErrorCode == SocketError.TimedOut)
+                    return false;
+                //if (ex.SocketErrorCode == SocketError.ConnectionRefused)
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ inet_t3/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+namespace inet_t3
+{
+    class Program
+    {
+        private static int _portStart = 0x0000;
+        private static int _portEnd = 0xffff;
+        private static IPAddress _ip = IPAddress.Loopback;
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                var s = args[i];
+                var ns = (args.Length > i+1) ? args[i + 1] : null;
+                ParseArg(s, ns);
+            }
+            Console.Title = "Preparing ...";
+            Console.WriteLine("Forming tasks ...");
+            var portChecker = new PortChecker(_ip);
+            var tasks = new List<ScanTask>();
+            for (var port = _portStart; port <= _portEnd; ++port)
+            {
+                var t = new ScanTask
+                {
+                    MyPort = port,
+                    Checker = portChecker,
+                    ResetEvent = new ManualResetEvent(false),
+                    UseUDP = false
+                };
+                ThreadPool.QueueUserWorkItem(a => ((ScanTask)a).Run(), t); 
+                tasks.Add(t);
+
+                t = new ScanTask
+                {
+                    MyPort = port,
+                    Checker = portChecker,
+                    ResetEvent = new ManualResetEvent(false),
+                    UseUDP = true
+                };
+                ThreadPool.QueueUserWorkItem(a => ((ScanTask)a).Run(), t); 
+                tasks.Add(t);
+            }
+            Console.WriteLine("Scanning ...");
+            var resetEvents = tasks.Select(task => (WaitHandle) task.ResetEvent).ToList();
+            var offset = 0;
+            while (resetEvents.Count > offset)
+            {
+                Console.Title = string.Format("Waiting threads {0}/{1} - {2:P}", 
+                    offset, resetEvents.Count,
+                    (float)offset / resetEvents.Count);
+                WaitHandle.WaitAll(resetEvents.Where((handle, i) => i >= offset && i < offset+64).ToArray());
+                offset += 64;
+            }
+
+            var tcpOpened = new List<int>();
+            var udpOpened = new List<int>();
+            foreach (var scanTask in tasks.Where(scanTask => scanTask.Result))
+            {
+                if (scanTask.UseUDP)
+                    udpOpened.Add(scanTask.MyPort);
+                else
+                    tcpOpened.Add(scanTask.MyPort);
+            }
+
+            Console.Title = "Complete";
+            Console.WriteLine("Opened TCP ports in range [{0}..{1}]:", _portStart, _portEnd);
+            Console.WriteLine(string.Join(", ", tcpOpened));
+            Console.WriteLine("Opened UDP ports in range [{0}..{1}]:", _portStart, _portEnd);
+            Console.WriteLine(string.Join(", ", udpOpened));
+        }
+
+        static void ParseArg(string arg, string nextArg)
+        {
+            ushort temp;
+            switch (arg)
+            {
+                case "/s":
+                    if (string.IsNullOrWhiteSpace(nextArg))
+                        return;
+                    if (!ushort.TryParse(nextArg, out temp))
+                        Console.WriteLine("Argument error: specified start port is not valid");
+                    _portStart = temp;
+                    return;
+                case "/e":
+                    if (string.IsNullOrWhiteSpace(nextArg))
+                        return;
+                    if (!ushort.TryParse(nextArg, out temp))
+                        Console.WriteLine("Argument error: specified end port is not valid");
+                    _portEnd = temp;
+                    return;
+                case "/ip":
+                    if (string.IsNullOrWhiteSpace(nextArg))
+                        return;
+                    if (!IPAddress.TryParse(nextArg, out _ip)) 
+                        Console.WriteLine("Argument error: specified ip is not valid");
+                    return;
+                case "/hostname":
+                    if (string.IsNullOrWhiteSpace(nextArg))
+                        return;
+                    try
+                    {
+                        var p = Dns.GetHostEntry(nextArg);
+                        _ip = p.AddressList.First();
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Argument error: specified hostname is not valid");
+                    }
+                    return;
+            }
+        }
+    }
+
+    class ScanTask
+    {
+        public int MyPort { get; set; }
+        public PortChecker Checker { get; set; }
+        public ManualResetEvent ResetEvent { get; set; }
+        public bool UseUDP { get; set; }
+        public bool Result { get; set; }
+
+        public void Run()
+        {
+            Result = UseUDP ? Checker.IsUdpOpened(MyPort) : Checker.IsTcpOpened(MyPort);
+            ResetEvent.Set();
+        }
+    }
+}
