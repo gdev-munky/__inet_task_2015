b412d2357729e7c7b4644536cf3055e959f6ea9d
Munky <ml4_aem@mail.ru>
2015-05-23 20:56:54 +0500

    [http-proxy] ();

---
+++ http-proxy/http-proxy/Connection.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpProxy
+{
+    public class Connection
+    {
+        public TcpClient Client { get; set; }
+        public TcpClient Server { get; set; }
+
+        public NetworkStream ClientStream { get; set; }
+        public NetworkStream ServerStream { get; set; }
+
+        public bool ShouldRun { get; set; }
+        public bool IsRunning { get; private set; }
+
+        public Connection(TcpClient client)
+        {
+            Client = client;
+            ShouldRun = true;
+            ClientStream = Client.GetStream();
+        }
+
+        public void DisconnectServer()
+        {
+            if (Server == null)
+                return;
+            Server.Close();
+            Server = null;
+            ServerStream = null;
+        }
+        public void DisconnectClient()
+        {
+            if (Client == null)
+                return;
+            Client.Close();
+            Client = null;
+            ClientStream = null;
+        }
+
+        public void Loop()
+        {
+            IsRunning = true;
+            var keepAlive = true;
+            while (ShouldRun)
+            {
+                if (ClientStream == null || !keepAlive)
+                {   
+                    ShouldRun = false;
+                    break;
+                }
+
+                var clientMessage = ClientStream.ReadAll();
+                if (clientMessage.Length < 0)
+                {
+                    Console.WriteLine("[ C ] : Disconnected");
+                    ShouldRun = false;
+                    break;
+                }
+                var httpRequest = HttpPacket.ReadRequest(clientMessage);
+                if (httpRequest == null)
+                {
+                    Console.WriteLine("[ C ] : Sent non HTTP. (length = {0})", clientMessage.Length);
+                    ShouldRun = false;
+                    break;
+                }
+                var method = httpRequest.Method;
+                var host = httpRequest.GetHeader("Host");
+                keepAlive = httpRequest.GetHeader("Connection", "").ToLowerInvariant() == "keep-alive";
+                Console.WriteLine("[ C ] : Got HTTP {1} request, {2}->{0}", host, method, Client.Client.RemoteEndPoint);
+
+                // <debug>
+                //Console.WriteLine(string.Join("; ", httpRequest.GetHeaders()));
+                // </debug>
+
+                if (Server == null && !TryConnectToServer(host))
+                {
+                    Console.WriteLine("[ C ] : Host {0} is unreachable", host);
+                    ShouldRun = false;
+                    break;
+                }
+                ServerStream.Write(clientMessage, 0, clientMessage.Length);
+                Console.WriteLine("[ C ] : Sent message to {0}", host);
+
+                // == Receiving server response =================================
+
+                var serverMessage = ServerStream.ReadAll();
+                if (serverMessage.Length < 0)
+                {
+                    Console.WriteLine("[ S ] : Disconnected");
+                    ShouldRun = false;
+                    break;
+                }
+                var httpResponse = HttpPacket.ReadResponse(serverMessage);
+                if (httpResponse == null)
+                {
+                    Console.WriteLine("[ S ] : Sent non HTTP. (length = {0})", serverMessage.Length);
+                    ShouldRun = false;
+                    break;
+                }
+
+                if (httpResponse.GetHeader("Connection", "").ToLowerInvariant() == "close")
+                    keepAlive = false;
+                Console.WriteLine("[ S ] : Got HTTP reponse {0}->{1} ({2})", Server.Client.RemoteEndPoint, Client.Client.RemoteEndPoint, httpResponse.ResultCode);
+                ClientStream.Write(serverMessage, 0, serverMessage.Length);
+                Console.WriteLine("[ S ] : Sent message to {0}", Client.Client.RemoteEndPoint);
+
+                //Console.WriteLine("Keep-Alive: " + keepAlive);
+            }
+            Console.WriteLine("[ ! ] Finished");
+            IsRunning = false;
+            DisconnectClient();
+            DisconnectServer();
+        }
+
+        private bool TryConnectToServer(string host)
+        {
+            try
+            {
+                Server = new TcpClient(host, 80);
+            }
+            catch (Exception)
+            {
+                Server = null;
+                return false;
+            }
+            ServerStream = Server.GetStream();
+            return true;
+        }
+    }
+
+    internal static class Ext
+    {
+        public static int BufferSize = 4096;
+        public static byte[] ReadAll(this NetworkStream stream)
+        {
+            var buffer = new byte[BufferSize];
+            var message = new List<byte>();
+            while (true)
+            {
+                int len;
+                try { len = stream.Read(buffer, 0, BufferSize); }
+                catch { break; }
+                if (len < 1) break;
+                message.AddRange(buffer.Take(len));
+                if (!stream.DataAvailable)
+                    break;
+            }
+            return message.ToArray();
+        }
+    }
+}
---
+++ http-proxy/http-proxy/HttpPacket.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HttpProxy
+{
+    public abstract class HttpPacket
+    {
+        public HttpPacket()
+        {
+            Content = new byte[0];
+            HttpVersion = "";
+        }
+
+        public string HttpVersion { get; protected set; }
+        public abstract string GetHeader(string key, string defaultValue = null);
+        public abstract IEnumerable<string> GetHeaders();
+        public byte[] Content { get; protected set; }
+
+        public abstract bool LoadFromBytes(byte[] bts);
+        public abstract byte[] GetBytes();
+
+        public static HttpRequestPacket ReadRequest(byte[] data)
+        {
+            var p = new HttpRequestPacket();
+            return p.LoadFromBytes(data) ? p : null;
+        }
+        public static HttpResponsePacket ReadResponse(byte[] data)
+        {
+            var p = new HttpResponsePacket();
+            return p.LoadFromBytes(data) ? p : null;
+        }
+    }
+
+    public class HttpRequestPacket : HttpPacket
+    {
+        public string Method { get; protected set; }
+        public string UriString { get; protected set; }
+        private readonly Dictionary<string, string> _headers = new Dictionary<string, string>();
+
+        public HttpRequestPacket()
+        {
+            Method = "";
+            UriString = "";
+        }
+
+        public override string GetHeader(string key, string defaultValue = null)
+        {
+            string value;
+            return _headers.TryGetValue(key, out value) ? value : defaultValue;
+        }
+
+        public override IEnumerable<string> GetHeaders()
+        {
+            return _headers.Keys;
+        }
+
+        public override bool LoadFromBytes(byte[] bts)
+        {
+            var ms = new MemoryStream(bts);
+            var sr = new StreamReader(ms);
+
+            var s = sr.ReadLine();
+            if (s == null) return false;
+            
+            var words = s.Split(' ');
+            if (words.Length < 3) return false;
+
+            Method = words[0].ToUpperInvariant();
+            UriString = words[1].ToLowerInvariant();
+            var httpv = words[2].ToUpperInvariant();
+            if (httpv.StartsWith("HTTP"))
+            {
+                var id = httpv.IndexOf("/", StringComparison.Ordinal);
+                if (id < 0 || id >= httpv.Length - 2)
+                    return false;
+                HttpVersion = httpv.Substring(id + 1);
+            }
+
+            while (true)
+            {
+                s = sr.ReadLine();
+                if (string.IsNullOrEmpty(s))
+                    break;
+
+                var id = s.IndexOf(":", StringComparison.Ordinal);
+                if (id < 0) continue;
+                _headers[s.Substring(0, id)] = s.Substring(id + 1).Trim();
+            }
+            var bytesLeft = (int) (ms.Length - ms.Position);
+            Content = new byte[bytesLeft];
+            ms.Read(Content, 0, bytesLeft);
+            return true;
+        }
+
+        public override byte[] GetBytes()
+        {
+            var ms = new MemoryStream();
+            var sw = new StreamWriter(ms);
+
+            sw.Write("{0} {1} HTTP/{2}\r\n", Method, UriString, HttpVersion);
+            foreach (var header in _headers)
+                sw.Write("{0}: {1}\r\n", header.Key, header.Value);
+            sw.Write("\r\n");
+            sw.Flush();
+            ms.Write(Content, 0, Content.Length);
+            ms.Flush();
+
+            return ms.ToArray();
+        }
+    }
+    public class HttpResponsePacket : HttpPacket
+    {
+        public override IEnumerable<string> GetHeaders()
+        {
+            return _headers.Keys;
+        }
+        public string ResultCode { get; protected set; }
+        public string ResultCodeDesc { get; protected set; }
+        private readonly Dictionary<string, string> _headers = new Dictionary<string, string>();
+
+        public HttpResponsePacket()
+        {
+            ResultCode = "";
+            ResultCodeDesc = "";
+        }
+
+        public override string GetHeader(string key, string defaultValue = null)
+        {
+            string value;
+            return _headers.TryGetValue(key, out value) ? value : defaultValue;
+        }
+
+        public override bool LoadFromBytes(byte[] bts)
+        {
+            var ms = new MemoryStream(bts);
+            var sr = new StreamReader(ms);
+
+            var s = sr.ReadLine();
+            if (s == null) return false;
+            
+            var words = s.Split(' ');
+            if (words.Length < 3) return false;
+
+            ResultCodeDesc = string.Join(" ", words.Skip(2));
+            ResultCode = words[1].ToLowerInvariant();
+            var httpv = words[0].ToUpperInvariant();
+            if (httpv.StartsWith("HTTP"))
+            {
+                var id = httpv.IndexOf("/", StringComparison.Ordinal);
+                if (id < 0 || id >= httpv.Length - 2)
+                    return false;
+                HttpVersion = httpv.Substring(id + 1);
+            }
+
+            while (true)
+            {
+                s = sr.ReadLine();
+                if (string.IsNullOrEmpty(s))
+                    break;
+
+                var id = s.IndexOf(":", StringComparison.Ordinal);
+                if (id < 0) continue;
+                _headers[s.Substring(0, id)] = s.Substring(id + 1).Trim();
+            }
+            var bytesLeft = (int) (ms.Length - ms.Position);
+            Content = new byte[bytesLeft];
+            ms.Read(Content, 0, bytesLeft);
+            return true;
+        }
+
+        public override byte[] GetBytes()
+        {
+            var ms = new MemoryStream();
+            var sw = new StreamWriter(ms);
+
+            sw.Write("HTTP/{0} {1} {2}\r\n", HttpVersion, ResultCode, ResultCodeDesc);
+            foreach (var header in _headers)
+                sw.Write("{0}: {1}\r\n", header.Key, header.Value);
+            sw.Write("\r\n");
+            sw.Flush();
+            ms.Write(Content, 0, Content.Length);
+            ms.Flush();
+            return ms.ToArray();
+        }
+    }
+}
